227f5178d4c69cd681c541062072a5ab3942db6c
Thinh NT <truongthinh.nguyen03@gmail.com>
2023-06-05 12:25:13 +0700

    update title

--- pages/2_Project_2.py
+++ pages/2_Project_2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title('NBA Player Stats Explorer')
+st.title('Project 2 - NBA Player Stats Explorer')
 
 st.markdown("""
 This app performs simple webscraping of NBA player stats data!
